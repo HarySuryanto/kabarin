92f92f4146415a09eb359181fdc5eefa42c26e32
harysuryanto <hary.suryanto01@gmail.com>
2020-09-30 19:21:44 +0700

    updated ui elements in login.dart and register.dart to match mockup

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -16,94 +16,107 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-        elevation: 0,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(20),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                // Icon person
-                Hero(
-                  tag: 'personIcon',
-                  child: Icon(
-                    Icons.person,
-                    size: 180,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Login',
+                    style: TextStyle(fontSize: 24),
                   ),
-                ),
-                
-                SizedBox(height: 20.0),
-                
-                // Username
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Username",
-                  ),
-                  keyboardType: TextInputType.text,
-                  validator: (val) => val.isEmpty ? 'Enter a username' : null,
-                  onChanged: (val) {
-                    setState(() => username = val);
-                  },
-                ),
-                
-                SizedBox(height: 20.0),
-                
-                // Password
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Password", 
-                  ),
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  validator: (val) => val.isEmpty ? 'Enter a password' : null,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
 
-                SizedBox(height: 60.0),
-                
-                // Button login
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: FlatButton(
-                    padding: EdgeInsets.all(16),
-                    color: Colors.blue[600],
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
-                        color: Colors.white
+                  SizedBox(height: 20),
+
+                  // Icon person
+                  Center(
+                    child: Hero(
+                      tag: 'personIcon',
+                      child: Icon(
+                        Icons.person,
+                        size: 180,
                       ),
                     ),
-                    onPressed: () {
-                      Toast.show('Username: $username\nPassword: $password', context, duration: Toast.LENGTH_LONG);
-                      Navigator.pushReplacementNamed(context, '/home');
-                    }
                   ),
-                ),
-                
-                SizedBox(height: 20),
-                
-                // Button register
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: FlatButton(
-                    padding: EdgeInsets.all(16),
-                    color: Colors.grey[300],
-                    child: Text(
-                      'Register'
+                  
+                  SizedBox(height: 20.0),
+                  
+                  // Username
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Username",
                     ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/register');
-                    }
+                    keyboardType: TextInputType.text,
+                    validator: (val) => val.isEmpty ? 'Enter a username' : null,
+                    onChanged: (val) {
+                      setState(() => username = val);
+                    },
                   ),
-                ),
-                
-              ],
+                  
+                  SizedBox(height: 20.0),
+                  
+                  // Password
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Password", 
+                    ),
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    validator: (val) => val.isEmpty ? 'Enter a password' : null,
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    },
+                  ),
+
+                  SizedBox(height: 60.0),
+                  
+                  // Button login
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: FlatButton(
+                      padding: EdgeInsets.all(16),
+                      color: Colors.blue[600],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        'LOGIN',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        Toast.show('Username: $username\nPassword: $password', context, duration: Toast.LENGTH_LONG);
+                        Navigator.pushReplacementNamed(context, '/home');
+                      }
+                    ),
+                  ),
+                  
+                  SizedBox(height: 20),
+                  
+                  // Button register
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: FlatButton(
+                      padding: EdgeInsets.all(16),
+                      color: Colors.grey[300],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        'REGISTER',
+                        style: TextStyle(color: Colors.grey[800],),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/register');
+                      }
+                    ),
+                  ),
+                  
+                ],
+              ),
             ),
           ),
         ),
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -14,119 +14,134 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Register"),
-        elevation: 0,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(20),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                // Icon person
-                Hero(
-                  tag: 'personIcon',
-                  child: Icon(
-                    Icons.person,
-                    size: 100,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Register',
+                    style: TextStyle(fontSize: 24),
                   ),
-                ),
-                
-                SizedBox(height: 20.0),
-                
-                // Username
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Username",
-                  ),
-                  keyboardType: TextInputType.text,
-                  validator: (val) => val.isEmpty ? 'Enter a username' : null,
-                  onChanged: (val) {
-                    setState(() => username = val);
-                  },
-                ),
-                
-                SizedBox(height: 20.0),
-                
-                // Password
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                  ),
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  validator: (val) => val.isEmpty ? 'Enter a password' : null,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
 
-                SizedBox(height: 20),
-                
-                // Fullname
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Fullname",
-                  ),
-                  keyboardType: TextInputType.text,
-                  validator: (val) => val.isEmpty ? 'Enter fullname' : null,
-                  onChanged: (val) {
-                    setState(() => username = val);
-                  },
-                ),
-                
-                SizedBox(height: 20.0),
-                
-                // Email
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                  ),
-                  keyboardType: TextInputType.text,
-                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                  onChanged: (val) {
-                    setState(() => username = val);
-                  },
-                ),
-                
-                SizedBox(height: 60.0),
-                
-                // Button register
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: FlatButton(
-                    padding: EdgeInsets.all(16),
-                    color: Colors.blue[600],
-                    child: Text(
-                      'Register',
-                      style: TextStyle(
-                        color: Colors.white
+                  SizedBox(height: 20),
+
+                  // Icon person
+                  Center(
+                    child: Hero(
+                      tag: 'personIcon',
+                      child: Icon(
+                        Icons.person_add,
+                        size: 100,
                       ),
                     ),
-                    onPressed: () {}
                   ),
-                ),
-                
-                SizedBox(height: 20),
-                
-                // Button cancel
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: FlatButton(
-                    padding: EdgeInsets.all(16),
-                    color: Colors.grey[300],
-                    child: Text(
-                      'Cancel'
+                  
+                  SizedBox(height: 20.0),
+                  
+                  // Username
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Username",
                     ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    }
+                    keyboardType: TextInputType.text,
+                    validator: (val) => val.isEmpty ? 'Enter a username' : null,
+                    onChanged: (val) {
+                      setState(() => username = val);
+                    },
                   ),
-                ),
-                
-              ],
+                  
+                  SizedBox(height: 20.0),
+                  
+                  // Password
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                    ),
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    validator: (val) => val.isEmpty ? 'Enter a password' : null,
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    },
+                  ),
+
+                  SizedBox(height: 20),
+                  
+                  // Fullname
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Fullname",
+                    ),
+                    keyboardType: TextInputType.text,
+                    validator: (val) => val.isEmpty ? 'Enter fullname' : null,
+                    onChanged: (val) {
+                      setState(() => username = val);
+                    },
+                  ),
+                  
+                  SizedBox(height: 20.0),
+                  
+                  // Email
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                    ),
+                    keyboardType: TextInputType.text,
+                    validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                    onChanged: (val) {
+                      setState(() => username = val);
+                    },
+                  ),
+                  
+                  SizedBox(height: 60.0),
+                  
+                  // Button register
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: FlatButton(
+                      padding: EdgeInsets.all(16),
+                      color: Colors.blue[600],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        'Register',
+                        style: TextStyle(
+                          color: Colors.white
+                        ),
+                      ),
+                      onPressed: () {}
+                    ),
+                  ),
+                  
+                  SizedBox(height: 20),
+                  
+                  // Button cancel
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: FlatButton(
+                      padding: EdgeInsets.all(16),
+                      color: Colors.grey[300],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        'Cancel',
+                        style: TextStyle(color: Colors.grey[800]),
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }
+                    ),
+                  ),
+                  
+                ],
+              ),
             ),
           ),
         ),
